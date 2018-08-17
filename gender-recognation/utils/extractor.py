c3becc4a7dc85fd5b1a7b48b79e71eb1abc418c7
hadi <hady.gharibi@gmail.com>
2018-08-17 23:36:47 +0430

    fix extractor bug

--- gender-recognation/utils/extractor.py
+++ gender-recognation/utils/extractor.py
@@ -1,10 +1,14 @@
 import os, sys, tarfile
 
 if __name__ == '__main__':
+    tar_path = os.getcwd()+'/../data/tar'
+    raw_path = os.getcwd()+'/../data/raw'
+    if not os.path.exists(raw_path) : os.mkdir(raw_path)
     files = os.listdir(os.getcwd()+'/../data/tar')
+
     for file in files:
-        tar = tarfile.open(file)
-        tar.extractall()
+        tar = tarfile.open(tar_path+'/' + file)
+        tar.extractall(raw_path+'/' + file)
         tar.close()
 
 
