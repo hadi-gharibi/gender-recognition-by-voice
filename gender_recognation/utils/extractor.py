56a875d32bdead2a81be168ba656a35bd7283de0
hadi <hady.gharibi@gmail.com>
2018-08-30 21:31:51 +0430

    add report

--- gender_recognation/utils/extractor.py
+++ gender_recognation/utils/extractor.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     module_path = os.path.dirname(gender_recognation.__file__)
     tar_path = os.path.join(module_path, 'data', 'tar')
     raw_path = os.path.join(module_path, 'data', 'raw')
-    if not os.path.exists(raw_path) : os.mkdir(raw_path)
+    if not os.path.exists(raw_path): os.mkdir(raw_path)
     files = os.listdir(tar_path)
 
     for file in files:
---
+++ report/README.md
@@ -0,0 +1,1 @@
+#### Reporting the result
